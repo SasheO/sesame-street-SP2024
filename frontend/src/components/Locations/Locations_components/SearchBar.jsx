9c7371a4c20a4462bdb77150e958d72a267a1ecd
MaryNkata <114526130+MaryNkata@users.noreply.github.com>
2025-02-03 12:36:22 -0500

    fixed typing bug in search bar

--- frontend/src/components/Locations/Locations_components/SearchBar.jsx
+++ frontend/src/components/Locations/Locations_components/SearchBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BiSearch, BiMicrophone } from 'react-icons/bi'; // Import icons
+import { BiSearch, BiMicrophone } from 'react-icons/bi';
+import './SearchBar.css';
 
 const SearchBar = () => {
   return (
     <div className="search-bar-container">
+      <input type="text" placeholder="Find a simple remedy" className="search-input" />
       <div className="icon-buttons">
         <BiSearch aria-label="Search icon" className="icon-btn" />
         <BiMicrophone aria-label="Microphone icon" className="icon-btn" />
       </div>
-      <input type="text" placeholder="Find a healthcare facility near you" className="search-input" />
     </div>
   );
 };
